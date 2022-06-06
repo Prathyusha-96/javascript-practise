e65de7f1a5f1367bdfac4fb8c0b9dbd53c0373a1
Prathyusha <89379190+Prathyusharam@users.noreply.github.com>
2022-06-06 21:51:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+function consoleNum() {
+  var isworkshop = true;
+  var num = 10;
+  if (isworkshop) {
+    let num = 20;
+    console.log(num);
+  }
+  console.log(num);
+}
+consoleNum();
+// concating
+// function sum(a, b) {
+//   return a + b; //12
+// }
+// sum(1,'2');
+
+// const sum = (a, b) =>  a + b;
+// console.log(sum (1, '2'))
+
+// let a = null;
+// let b;
+
+// console.log(a==b);
+// console.log(a===b);
+// {
+//   var a = 2;
+//   var a = 3;
+//   console.log(a);
+// }
+//  {
+//    let b = 5;
+//    let b = 9;
+//    console.log(b);
+//  }
