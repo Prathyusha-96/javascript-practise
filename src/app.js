5150d1d3437daea209981c0b3d2d7d37cfe678c9
Prathyusha <89379190+Prathyusharam@users.noreply.github.com>
2022-06-07 18:00:26 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ const strLength = (name, cb) => {
   const lengthOfName = name.length;
   cb(lengthOfName);
 };
-const printName = (nameLength) => console.log(`my name is ${nameLength}long`);
-printName(5);
-strLength("pinky", printName);
+strLength("ram");
+// const printName = (nameLength) => console.log(`my name is ${nameLength}long`);
+// printName(5);
+// strLength("pinky", printName);
+
+// undefined vs not defined
+console.log(a);
+var a = 10;
+{
+  var b = 5;
+  console.log(b);
+}
